2084f601ace81a7b707c34a3482cca46323b9a57
Bianca Oura <biancaoura@gmail.com>
2022-08-16 16:30:46 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('testing NotFound component', () => {
+  test('if there\'s a header with the correct text', () => {
+    render(<NotFound />);
+
+    const notFoundHeader = screen
+      .getByRole('heading', { level: 2 });
+    expect(notFoundHeader).toHaveTextContent('Page requested not found');
+    expect(notFoundHeader).toBeInTheDocument();
+  });
+
+  test('if renders correct image (pikachu crying)', () => {
+    render(<NotFound />);
+
+    const pikachuImg = screen
+      .getByRole('img',
+        { name: 'Pikachu crying because the page requested was not found' });
+    expect(pikachuImg.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
