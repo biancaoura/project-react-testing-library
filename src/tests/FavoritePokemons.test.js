18cb028c02089ab60feabc842e7dc96f1434239a
Bianca Oura <biancaoura@gmail.com>
2022-08-16 16:25:35 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('testing FavoritePokemons component', () => {
+  test('if renders correct message if there\'s any favorite pokémon', () => {
+    render(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
